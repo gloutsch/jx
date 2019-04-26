364cc03b4d8dafac56c04c79f61f2b937ffba6bc
Gauthier Loutsch <gauthier.loutsch@contentsquare.com>
2019-04-26 16:16:24 +0200

    use native tags, not eksctl tags

--- pkg/jx/cmd/get_eks.go
+++ pkg/jx/cmd/get_eks.go
@@ -104,9 +104,9 @@ func (o *GetEksOptions) Run() error {
 		instances, err := svc.DescribeInstances(&ec2.DescribeInstancesInput{
 			Filters: []*ec2.Filter{
 				{
-					Name: aws.String("tag:eksctl.cluster.k8s.io/v1alpha1/cluster-name"),
+					Name: aws.String("tag:kubernetes.io/cluster/" + cluster),
 					Values: []*string{
-						aws.String(cluster),
+						aws.String("owned"),
 					},
 				},
 			},
